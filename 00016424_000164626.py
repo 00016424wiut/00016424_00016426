92a9f6502dcdaead8e13adb7bc1c1965a3a20595
00016424wiut@gmail.com <00016424wiut@gmail.com>
2023-11-09 15:56:49 +0500

    data cleaning

--- 00016424_000164626.py
+++ 00016424_000164626.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import seaborn as sb
 data = pd.read_csv("graduation_dataset.csv")
-print(data.head())
 
-#calculating the average of specific column
+
+#data cleaning
+print(data.isnull().sum())
+data = data.drop_duplicates()
+data = data.fillna(0)
+
