e793339723e37f432554997a05a9674756dceaa4
00016424 <00016424wiut@gmail.com>
2023-11-09 17:11:45 +0500

    another commit

--- 00016424_000164626.py
+++ 00016424_000164626.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import seaborn as sb
+import matplotlib.pyplot as plt
+import argparse
 data = pd.read_csv("graduation_dataset.csv")
 
 
@@ -8,3 +10,9 @@ print(data.isnull().sum())
 data = data.drop_duplicates()
 data = data.fillna(0)
 
+#data analysis/creating a visualization
+print(data.describe())
+data['Martial status'].value_counts().plot(kind='bar')
+plt.show()
+
+#user interface
