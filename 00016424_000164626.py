def3834778d56aece304e45467ff6a93b9c64a0f
00016426 <00016426@gmail.com>
2023-11-11 16:08:08 +0500

    init:my first commit

--- 00016424_000164626.py
+++ 00016424_000164626.py
@@ -2,6 +2,8 @@ import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 import argparse
+import tkinter as tk
+
 data = pd.read_csv("graduation_dataset.csv")
 
 
@@ -16,3 +18,14 @@ data['Martial status'].value_counts().plot(kind='bar')
 plt.show()
 
 #user interface
+window = tk.Tk()
+label = tk.Label(window, text="Welcome to the site!")
+label.pack()
+window.mainloop()
+
+
+parser = argparse.ArgumentParser(description='Sample CLI using argparse')
+parser.add_argument("name", help="Your name")
+parser.add_argument("age", help="Your age", type=int)
+args = parser.parse_args()
+print(f"Hello, {args.name}. You are {args.age} years old.")
